6257a397d48cb379f24626927e31a25720fcd3a3
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-06-17 22:05:21 +0300

    set up sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import { MdOutlineDashboard, MdAccountCircle, MdAnalytics, MdOutlineSettings, MdLogout} from 'react-icons/md';
+import { BsChevronDown, BsChatLeftText, BsCalendarCheck, BsFiles, BsServer } from 'react-icons/bs';
+
+const Menus = [
+];
+
+const App = () => {
+}
+
+export default App
+
